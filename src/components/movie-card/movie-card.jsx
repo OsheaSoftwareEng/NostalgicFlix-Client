bf4934bac4f3c11c77fb89a05eada09b49260746
Oshea Sanders <osheasoftwareeng@gmail.com>
2023-05-28 12:45:37 -0400

    added proptype conditions to for MovieCard

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,3 +1,5 @@
+import PropTypes from 'prop-types';
+
 //code for div that shows movie title and is clickable
 export const MovieCard = ({ movie, onMovieClick }) => {
   return (
@@ -6,7 +8,15 @@ export const MovieCard = ({ movie, onMovieClick }) => {
         onMovieClick(movie);
       }}
     >
-      {movie.title}
+      {movie.Title}
     </div>
   );
+};
+
+//PropTypes conditions for return MovieCard statement in main-view.jsx
+MovieCard.propTypes = {
+  movie: PropTypes.shape({
+    Title: PropTypes.string.isRequired
+  }).isRequired,
+  onMovieClick: PropTypes.func.isRequired
 };
