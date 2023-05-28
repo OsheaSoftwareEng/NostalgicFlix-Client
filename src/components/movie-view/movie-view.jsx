f011eadffd6a9aa312c2653614ea1793cdc12605
Oshea Sanders <osheasoftwareeng@gmail.com>
2023-05-28 12:47:10 -0400

    updated MovieView information to extract from API and add proptype conditions

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -1,29 +1,52 @@
+import PropTypes from 'prop-types';
+
 //information for movie information displayed once user clicks a movie
 export const MovieView = ({ movie, onBackClick }) => {
   return (
     <div>
       <div>
-        <img src={movie.image} />
+        <img src={movie.ImagePath} />
       </div>
       <div>
         <div>
           <span>Title: </span>
-          <span>{movie.title}</span>
+          <span>{movie.Title}</span>
         </div>
         <div>
           <span>Genre: </span>
-          <span>{movie.genre.name}</span>
+          <span>{movie.Genre.Name}</span>
         </div>
         <div>
           <span>Description: </span>
-          <span>{movie.description}</span>
+          <span>{movie.Description}</span>
         </div>
         <div>
           <span>Director: </span>
-          <span>{movie.director.name}</span>
+          <span>{movie.Director.Name}</span>
+        </div>
+        <div>
+          <span>Featured: </span>
+          <span>{movie.Featured}</span>
         </div>
         <button onClick={onBackClick}>Back</button>
       </div>
     </div>
   );
+};
+
+//PropType conditions for moviesFromAPI variable return statement in main-view.jsx
+MovieView.propTypes = {
+  movie: PropTypes.shape({
+    ImagePath: PropTypes.string.isRequired,
+    Title: PropTypes.string.isRequired,
+    Genre: PropTypes.shape({
+      Name: PropTypes.string.isRequired
+    }),
+    Description: PropTypes.string.isRequired,
+    Director: PropTypes.shape({
+      Name: PropTypes.string.isRequired
+    }),
+    Featured: PropTypes.bool.isRequired
+  }).isRequired,
+  onBackClick: PropTypes.func.isRequired
 };
