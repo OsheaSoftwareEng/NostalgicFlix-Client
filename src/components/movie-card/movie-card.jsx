5e45373a6d5f5dd370f6c0b38456f1b0aefc31bf
Oshea Sanders <osheasoftwareeng@gmail.com>
2023-06-04 10:34:21 -0400

    added bootstrap button and card to movie card component

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,15 +1,24 @@
 import PropTypes from 'prop-types';
+import { Button, Card } from 'react-bootstrap';
+import './movie-card.scss';
 
 //code for div that shows movie title and is clickable
 export const MovieCard = ({ movie, onMovieClick }) => {
   return (
-    <div
-      onClick={() => {
-        onMovieClick(movie);
-      }}
-    >
-      {movie.Title}
-    </div>
+    <Card className='h-100 card text-bg-dark mb-3'>
+      <Card.Img className='w-100' variant='top' src={movie.ImagePath} />
+      <Card.Body>
+        <Card.Title>{movie.Title}</Card.Title>
+        <Card.Text>{movie.Description}</Card.Text>
+        <Button
+          className='open-button'
+          onClick={() => onMovieClick(movie)}
+          variant='outline-info'
+        >
+          Open
+        </Button>
+      </Card.Body>
+    </Card>
   );
 };
 
