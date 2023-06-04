58ae480d229c4884ab82b69a54e907d36e5d1bbc
Oshea Sanders <osheasoftwareeng@gmail.com>
2023-06-04 10:27:46 -0400

    converted code and added bootstrap to code

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -3,6 +3,10 @@ import { MovieCard } from '../movie-card/movie-card.jsx';
 import { MovieView } from '../movie-view/movie-view.jsx';
 import { LoginView } from '../login-view/login-view.jsx';
 import { SignupView } from '../signup-view/signup-view.jsx';
+import { Button } from 'react-bootstrap';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import './main-view.scss';
 
 export const MainView = () => {
   const storedUser = JSON.parse(localStorage.getItem('user'));
@@ -43,57 +47,94 @@ export const MainView = () => {
       });
   }, [token]);
 
-  //this returns a login view making the user have to login in before being able to use the application.
   if (!user) {
     return (
-      <>
-        <LoginView
-          onLoggedIn={(user, token) => {
-            setUser(user);
-            setToken(token);
-          }}
-        />
-        or
-        <SignupView />
-      </>
+      <Row className='justify-content-md-center'>
+        <Col md={5} className='form-bg-style'>
+          <LoginView
+            onLoggedIn={(user, token) => {
+              setUser(user);
+              setToken(token);
+            }}
+          />
+          <span>OR</span>
+          <SignupView />
+        </Col>
+      </Row>
     );
   }
 
-  //statement for movies selected to show movie view details and includes code for when clicking the back button to go to list of movies.
-  if (selectedMovie) {
-    return (
-      <MovieView
-        movie={selectedMovie}
-        onBackClick={() => setSelectedMovie(null)}
-      />
-    );
-  }
-  //if theres no movies in the array the page will say the list is empty
-  if (movies.length === 0) {
-    return <div>The list is empty!</div>;
-  }
+  //this returns a login view making the user have to login in before being able to use the application.
 
-  //return statement for movies in the array being displayed and being clickable from MovieCard file
   return (
-    <div>
-      {movies.map((movie) => (
-        <MovieCard
-          key={movie.Title}
-          movie={movie}
-          onMovieClick={(newSelectedMovie) => {
-            setSelectedMovie(newSelectedMovie);
-          }}
-        />
-      ))}
-      <button
-        onClick={() => {
-          setUser(null);
-          setToken(null);
-          localStorage.clear();
-        }}
-      >
-        Logout
-      </button>
-    </div>
+    <Row className='justify-content-md-center'>
+      {!user ? (
+        <>
+          <LoginView onLoggedIn={(user) => setUser(user)} />
+          or
+          <SignupView />
+        </>
+      ) : selectedMovie ? (
+        <Col md={3}>
+          <MovieView
+            movie={selectedMovie}
+            onBackClick={() => setSelectedMovie(null)}
+          />
+        </Col>
+      ) : movies.length === 0 ? (
+        <div>The list is empty!</div>
+      ) : (
+        <>
+          {movies.map((movie) => (
+            <Col className='mb-5' key={movie.Title} md={3}>
+              <MovieCard
+                movie={movie}
+                onMovieClick={(newSelectedMovie) => {
+                  setSelectedMovie(newSelectedMovie);
+                }}
+              />
+            </Col>
+          ))}
+          <Button
+            variant='danger'
+            onClick={() => {
+              setUser(null);
+              setToken(null);
+              localStorage.clear();
+            }}
+          >
+            Logout
+          </Button>
+        </>
+      )}
+    </Row>
   );
 };
+
+//statement for movies selected to show movie view details and includes code for when clicking the back button to go to list of movies.
+
+//   if (selectedMovie) {
+//     return (
+//       <MovieView
+//         movie={selectedMovie}
+//         onBackClick={() => setSelectedMovie(null)}
+//       />
+//     );
+//   }
+//   //if theres no movies in the array the page will say the list is empty
+//   if (movies.length === 0) {
+//     return <div>The list is empty!</div>;
+//   }
+
+//   //return statement for movies in the array being displayed and being clickable from MovieCard file
+//   return (
+//     <div>
+//       {movies.map((movie) => (
+//         <MovieCard
+//           key={movie.Title}
+//           movie={movie}
+//           onMovieClick={(newSelectedMovie) => {
+//             setSelectedMovie(newSelectedMovie);
+//           }}
+//         />
+//       ))}
