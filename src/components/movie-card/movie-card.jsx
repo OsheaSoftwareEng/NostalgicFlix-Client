9901b55d350cdc1813a17b1a4dfafe812030efd9
Oshea Sanders <osheasoftwareeng@gmail.com>
2023-06-06 08:27:05 -0400

    added link from react router to link to movies

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,18 +1,19 @@
 import PropTypes from 'prop-types';
 import { Button, Card } from 'react-bootstrap';
 import './movie-card.scss';
+import { Link } from 'react-router-dom';
 
 //code for div that shows movie title and is clickable
-export const MovieCard = ({ movie, onMovieClick }) => {
+export const MovieCard = ({ movie }) => {
   return (
     <Card className='h-100 card text-bg-dark mb-3'>
       <Card.Img className='w-100' variant='top' src={movie.ImagePath} />
       <Card.Body>
         <Card.Title>{movie.Title}</Card.Title>
         <Card.Text>{movie.Description}</Card.Text>
-        <Button onClick={() => onMovieClick(movie)} variant='outline-info'>
-          See More
-        </Button>
+        <Link to={`/movies${movie.Title}`}>
+          <Button variant='outline-info'>See More</Button>
+        </Link>
       </Card.Body>
     </Card>
   );
@@ -23,6 +24,5 @@ MovieCard.propTypes = {
   movie: PropTypes.shape({
     Title: PropTypes.string.isRequired,
     Description: PropTypes.string.isRequired
-  }).isRequired,
-  onMovieClick: PropTypes.func.isRequired
+  }).isRequired
 };
