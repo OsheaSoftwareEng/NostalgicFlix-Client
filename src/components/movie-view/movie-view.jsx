8a2303bf8e547b1b6b01b94064a3edf9209badbc
Oshea Sanders <osheasoftwareeng@gmail.com>
2023-06-07 11:39:32 -0400

    converted movieview code to bootstrap

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -1,58 +1,88 @@
 import PropTypes from 'prop-types';
 import './movie-view.scss';
-import { Button } from 'react-bootstrap';
+import { Button, Col, Row } from 'react-bootstrap';
 import { Link, useParams } from 'react-router-dom';
 
 //information for movie information displayed once user clicks a movie
-export const MovieView = ({ movie }) => {
-  // const { movieId: currentMovieId } = useParams();
-  // const movie = movies.find((movie) => movie.id === currentMovieId);
+export const MovieView = ({ movieInfo }) => {
+  const { movieId } = useParams();
+  const Movie = movieInfo.find((movie) => movie._id === movieId);
   return (
-    <div>
-      <div>
-        <img className='w-100 h-100' src={movie.ImagePath} />
-      </div>
-      <div>
-        <div>
+    <>
+      <Row>
+        <Col>
+          <div className='ratio ratio-16x9'>
+            <iframe
+              src='https://www.youtube.com/embed/Ouiy3WUJk-A'
+              title='Youtube video'
+              allowfullscreen
+            >
+              {' '}
+            </iframe>
+          </div>
+          {/* <img className='w-100 h-100' src={Movie.ImagePath} /> */}
+        </Col>
+      </Row>
+      <Row>
+        <Col>
           <span>Title: </span>
-          <span>{movie.Title}</span>
-        </div>
-        <div>
-          <span>Genre: </span>
-          <span>{movie.Genre.Name}</span>
-        </div>
-        <div>
+          <span>{Movie.Title}</span>
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          <span>Genre:</span>
+          <span>{Movie.Genre.Name}</span>
+        </Col>
+      </Row>
+      <Row>
+        <Col>
           <span>Description: </span>
-          <span>{movie.Description}</span>
-        </div>
-        <div>
+          <span>{Movie.Description}</span>
+        </Col>
+      </Row>
+      <Row>
+        <Col>
           <span>Director: </span>
-          <span>{movie.Director.Name}</span>
-        </div>
-        <div>
+          <span>{Movie.Director.Name}</span>
+        </Col>
+      </Row>
+      <Row>
+        <Col>
           <span>Featured: </span>
-          <span>{movie.Featured}</span>
-        </div>
-        <Link to={`/`}>
-          <Button variant='outline-info mt-1'>Back</Button>
-        </Link>
-      </div>
-    </div>
+          <span>{Movie.Featured}</span>
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          <Link to={`/`}>
+            <Button variant='outline-info mt-1'>Back</Button>
+          </Link>
+        </Col>
+        <Col>
+          <Link target='_blank' to={movieInfo._id}>
+            <Button className='watch-button' variant='outline-danger mt-1'>
+              Watch Now
+            </Button>
+          </Link>
+        </Col>
+      </Row>
+    </>
   );
 };
 
 //PropType conditions for moviesFromAPI variable return statement in main-view.jsx
-MovieView.propTypes = {
-  movie: PropTypes.shape({
-    ImagePath: PropTypes.string.isRequired,
-    Title: PropTypes.string.isRequired,
-    Genre: PropTypes.shape({
-      Name: PropTypes.string.isRequired
-    }),
-    Description: PropTypes.string.isRequired,
-    Director: PropTypes.shape({
-      Name: PropTypes.string.isRequired
-    }),
-    Featured: PropTypes.string.isRequired
-  }).isRequired
-};
+// MovieView.propTypes = {
+//   movie: PropTypes.shape({
+//     ImagePath: PropTypes.string.isRequired,
+//     Title: PropTypes.string.isRequired,
+//     Genre: PropTypes.shape({
+//       Name: PropTypes.string.isRequired
+//     }),
+//     Description: PropTypes.string.isRequired,
+//     Director: PropTypes.shape({
+//       Name: PropTypes.string.isRequired
+//     }),
+//     Featured: PropTypes.string.isRequired
+//   }).isRequired
+// };
