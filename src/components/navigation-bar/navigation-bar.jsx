71b21fe0074c16817dff8a01979f813150f78d42
Oshea Sanders <osheasoftwareeng@gmail.com>
2023-06-18 10:22:15 -0400

    updated nav bar to be hidden until user login

--- src/components/navigation-bar/navigation-bar.jsx
+++ src/components/navigation-bar/navigation-bar.jsx
@@ -1,16 +1,31 @@
 import { Navbar, Container, Nav } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
+import { Button } from 'react-bootstrap';
+import { useState } from 'react';
 
-export const NavigationBar = ({ user, onLoggedOut }) => {
+export const NavigationBar = ({ user, setUser, setToken }) => {
+  if (!user) {
+    return null;
+  }
+
   return (
     <>
-      <Navbar bg='light' variant='light'>
+      <Navbar bg='info' variant='info'>
         <Container>
           <Navbar.Brand href='/'></Navbar.Brand>
           <Nav className='me-auto'>
             <Nav.Link href='/'>Home</Nav.Link>
-            <Nav.Link href='/movies/Title'>Features</Nav.Link>
-            <Nav.Link href='#pricing'>Pricing</Nav.Link>
+            <Nav.Link href='/users'>Profile</Nav.Link>
+            <Nav.Link
+              href='#'
+              onClick={() => {
+                setUser(null);
+                setToken(null);
+                localStorage.clear();
+              }}
+            >
+              Logout
+            </Nav.Link>
           </Nav>
         </Container>
       </Navbar>
