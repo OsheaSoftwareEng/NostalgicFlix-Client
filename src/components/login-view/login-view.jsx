caa4ae0af277b0a717067c53897de41354d21b57
Oshea Sanders <osheasoftwareeng@gmail.com>
2023-06-26 13:13:15 -0400

    added sweetalert for sucess and fail alerts added new ui for login screen

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -3,6 +3,12 @@ import { useState } from 'react';
 import Form from 'react-bootstrap/Form';
 import { Button } from 'react-bootstrap';
 import './login-view.scss';
+import { BsFillPersonFill } from 'react-icons/bs';
+import InputGroup from 'react-bootstrap/InputGroup';
+import { AiFillLock } from 'react-icons/ai';
+import { Row, Col } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import Swal from 'sweetalert2';
 
 export const LoginView = ({ onLoggedIn }) => {
   const [username, setUsername] = useState('');
@@ -10,6 +16,18 @@ export const LoginView = ({ onLoggedIn }) => {
 
   const handleSubmit = (event) => {
     event.preventDefault();
+
+    const Toast = Swal.mixin({
+      toast: true,
+      position: 'top-end',
+      showConfirmButton: false,
+      timer: 2000,
+      timerProgressBar: true,
+      didOpen: (toast) => {
+        toast.addEventListener('mouseenter', Swal.stopTimer);
+        toast.addEventListener('mouseleave', Swal.resumeTimer);
+      }
+    });
 
     const data = {
       Username: username,
@@ -30,8 +48,15 @@ export const LoginView = ({ onLoggedIn }) => {
           localStorage.setItem('user', JSON.stringify(data.user));
           localStorage.setItem('token', data.token);
           onLoggedIn(data.user, data.token);
+          Toast.fire({
+            icon: 'success',
+            title: 'Signed in successfully'
+          });
         } else {
-          alert('No such user');
+          Toast.fire({
+            icon: 'error',
+            title: "User doesn't exist"
+          });
         }
       })
       .catch((e) => {
@@ -40,33 +65,59 @@ export const LoginView = ({ onLoggedIn }) => {
   };
 
   return (
-    <Form onSubmit={handleSubmit}>
-      <Form.Group controlId='formUsername'>
-        <Form.Label>Username:</Form.Label>
-        <Form.Control
-          type='text'
-          value={username}
-          onChange={(e) => setUsername(e.target.value)}
-          required
-          minLength='3'
-        />
-      </Form.Group>
-      <Form.Group controlId='formPassword'>
-        <Form.Label>Password:</Form.Label>
-        <Form.Control
-          type='password'
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          required
-        />
-      </Form.Group>
-      <Button
-        className='mt-1 d-grid gap-2 col-12 mx-auto'
-        variant='outline-info'
-        type='submit'
-      >
-        Log in
-      </Button>
-    </Form>
+    <>
+      <Form id='box-login' onSubmit={handleSubmit}>
+        <div id='form-login'>
+          <h2 className='font-style'>Login</h2>
+          <Form.Group controlId='formUsername'>
+            <Form.Label></Form.Label>
+            <InputGroup>
+              <Form.Control
+                type='text'
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+                required
+                minLength='3'
+                placeholder='Username'
+              />
+              <InputGroup.Text id='input-style-login'>
+                <BsFillPersonFill size={25} className='user-icon' />
+              </InputGroup.Text>
+            </InputGroup>
+          </Form.Group>
+          <Form.Group controlId='formPassword'>
+            <Form.Label></Form.Label>
+            <InputGroup>
+              <Form.Control
+                id=''
+                type='password'
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                required
+                placeholder='Password'
+              />
+              <InputGroup.Text id='input-style-login'>
+                <AiFillLock size={25} className='user-icon' />
+              </InputGroup.Text>
+            </InputGroup>
+          </Form.Group>
+          <Button
+            id='btn-login-nf'
+            className='mt-2 col-10 font-style'
+            type='submit'
+          >
+            Log In
+          </Button>
+          <Row>
+            <Col className='mt-2'>
+              <span className='font-style'>Not a member?</span>{' '}
+              <Link to={'/signup'} className='link-style-login font-style'>
+                Signup
+              </Link>
+            </Col>
+          </Row>
+        </div>
+      </Form>
+    </>
   );
 };
