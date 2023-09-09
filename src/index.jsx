d27ecd301dfa39b9a70d1b20ed15e5b55818cf89
Oshea Sanders <osheasoftwareeng@gmail.com>
2023-09-09 13:21:32 -0400

    added pwa requirements to be an app

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,8 @@
 import { createRoot } from 'react-dom/client';
 import { MainView } from './components/main-view/main-view.jsx';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration.js';
+import reportWebVitals from './reportWebVitals';
 import './index.scss';
-import { Container } from 'react-bootstrap';
 
 //Main component (will eventually use all the others)
 export const App = () => {
@@ -14,3 +15,13 @@ const root = createRoot(container);
 
 //Tells react to render your app in the root DOM element
 root.render(<App />);
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.register();
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
