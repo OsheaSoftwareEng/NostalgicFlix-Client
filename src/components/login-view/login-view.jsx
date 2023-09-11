e5f02979a290754accb69250688e9e55d519424a
Oshea Sanders <osheasoftwareeng@gmail.com>
2023-09-11 10:47:37 -0400

    added guest sign option

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -23,6 +23,10 @@ export const LoginView = ({ onLoggedIn, user, token }) => {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
 
+  //Guest sign in info
+  const userName = 'testuser12345';
+  const passWord = 'legend5000';
+
   const handleSubmit = (event) => {
     event.preventDefault();
 
@@ -49,6 +53,58 @@ export const LoginView = ({ onLoggedIn, user, token }) => {
         'Content-Type': 'application/json'
       },
       body: JSON.stringify(data)
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log('Login response: ', data);
+        if (data.user) {
+          localStorage.setItem('user', JSON.stringify(data.user));
+          localStorage.setItem('token', data.token);
+          onLoggedIn(data.user, data.token);
+          Toast.fire({
+            icon: 'success',
+            title: 'Signed in successfully'
+          });
+          window.location.reload();
+        } else {
+          Toast.fire({
+            icon: 'error',
+            title: "User doesn't exist"
+          });
+        }
+      })
+      .catch((e) => {
+        alert(e);
+      });
+  };
+
+  //Guest login section
+  const guestHandleSubmit = (event) => {
+    event.preventDefault();
+
+    const Toast = Swal.mixin({
+      toast: true,
+      position: 'top-end',
+      showConfirmButton: false,
+      timer: 2000,
+      timerProgressBar: true,
+      didOpen: (toast) => {
+        toast.addEventListener('mouseenter', Swal.stopTimer);
+        toast.addEventListener('mouseleave', Swal.resumeTimer);
+      }
+    });
+
+    const userData = {
+      Username: userName,
+      Password: passWord
+    };
+
+    fetch('https://nostalgic-flix.herokuapp.com/login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(userData)
     })
       .then((response) => response.json())
       .then((data) => {
@@ -118,6 +174,14 @@ export const LoginView = ({ onLoggedIn, user, token }) => {
             type='submit'
           >
             Log In
+          </Button>
+          <Button
+            id='btn-login-nf'
+            className='mt-2 col-10 font-style'
+            type='submit'
+            onClick={guestHandleSubmit}
+          >
+            Continue As Guest
           </Button>
           <Row>
             <Col>
